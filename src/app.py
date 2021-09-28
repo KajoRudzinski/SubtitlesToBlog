ccc867a7071c64836ea8ada4b9745b2803c8f2de
Kajo Rudziński <48604487+KajoRudzinski@users.noreply.github.com>
2021-09-28 23:47:43 +0200

    reading path for srt

--- src/app.py
+++ src/app.py
@@ -1,2 +1,17 @@
-def read_path():
-    return ''
+import os
+
+
+def read_path(path: str) -> str:
+    """Reads folder with SRT files containing subtitles
+    and returns path to first found SRT file"""
+    path_content = os.listdir(path)
+    srt_file = ''
+    for f in path_content:
+        if f[-4:] == '.srt':
+            srt_file = f
+            break
+
+    if srt_file == '':
+        return 'No SRT File'
+    else:
+        return os.path.join(path, srt_file)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,18 @@
 from src import app
 
+import os
 
-def test_read_path():
-    assert app.read_path() != None
+has_srt = "has_srt\\"
+has_no_srt = "has_no_srt\\"
 
+
+def test_read_path_no_srt():
+    p = os.path.join(os.getcwd(), has_no_srt)
+    rp = app.read_path(p)
+    assert rp == 'No SRT File'
+
+
+def test_read_path_has_srt():
+    p = os.path.join(os.getcwd(), has_srt)
+    rp = app.read_path(p)
+    assert rp == os.path.join(os.getcwd(), has_srt, 'test.srt')
